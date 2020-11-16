45694d3181501f05a7696a13d5da82013381d61d
Shanil Lobanwala <slobanwala@ad.thinkmcs.com>
2020-11-16 03:23:03 -0600

    Fixed PK

--- my_submission/Data Engineering/table_schema.sql
+++ my_submission/Data Engineering/table_schema.sql
@@ -48,7 +48,7 @@ CREATE TABLE salaries (
 CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
 	dept_no VARCHAR(30) NOT NULL,
-	PRIMARY KEY(emp_no),
+	PRIMARY KEY(emp_no, dept_no),
 	FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
 	FOREIGN KEY(dept_no) REFERENCES departments(dept_no)
 );
