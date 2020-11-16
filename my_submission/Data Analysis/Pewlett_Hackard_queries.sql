21499529e6bf1e4c8742c958d1382da982c4da09
Shanil Lobanwala <slobanwala@ad.thinkmcs.com>
2020-11-16 04:04:23 -0600

    Finished part 8

--- my_submission/Data Analysis/Pewlett_Hackard_queries.sql
+++ my_submission/Data Analysis/Pewlett_Hackard_queries.sql
@@ -56,4 +56,16 @@ WHERE departments.dept_name = 'Sales'
 ORDER BY emp_no;
 
 
+--7.List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+LEFT JOIN dept_emp
+ON employees.emp_no = dept_emp.emp_no
+LEFT JOIN departments
+ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name IN('Sales', 'Development')
+ORDER BY emp_no;
+
+--8.
+
 
