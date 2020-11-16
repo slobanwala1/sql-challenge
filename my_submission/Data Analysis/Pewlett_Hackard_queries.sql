678353e5a1e17f9ad3b83f0498495538615e957b
Shanil Lobanwala <slobanwala@ad.thinkmcs.com>
2020-11-16 04:08:41 -0600

    Added name and title to sql file

--- my_submission/Data Analysis/Pewlett_Hackard_queries.sql
+++ my_submission/Data Analysis/Pewlett_Hackard_queries.sql
@@ -1,9 +1,6 @@
--- SELECT * FROM departments
--- SELECT * FROM dept_emp
--- SELECT * FROM dept_manager
--- SELECT * FROM employees
--- SELECT * FROM salaries
--- SELECT * FROM titles
+-- SQL Data Analysis portion
+-- Author: Shanil Lobanwala
+
 
 -- 1.List the following details of each employee: employee number, last name, first name, sex, and salary.
 SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary 
