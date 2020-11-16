9cbf04aa27dd1620be6bfa6cb26735d38bc100a9
Shanil Lobanwala <slobanwala@ad.thinkmcs.com>
2020-11-16 04:07:40 -0600

    Finished part 8

--- my_submission/Data Analysis/Pewlett_Hackard_queries.sql
+++ my_submission/Data Analysis/Pewlett_Hackard_queries.sql
@@ -66,6 +66,11 @@ ON dept_emp.dept_no = departments.dept_no
 WHERE departments.dept_name IN('Sales', 'Development')
 ORDER BY emp_no;
 
---8.
+
+--8.In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, COUNT(*) AS freq_count
+FROM employees
+GROUP BY last_name
+ORDER BY freq_count DESC;
 
 
